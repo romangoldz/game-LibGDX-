f7108aafb1aebff56729cdc7f99132365ba12380
romangoldz <roman.goldzitskiy.2011@gmail.com>
2026-09-16 23:10:58 +0300

    Refactor Main class to use ApplicationListener
    Refactored Main class to implement ApplicationListener and removed Game class inheritance.

--- core/src/main/java/com/mygame/Main.java
+++ core/src/main/java/com/mygame/Main.java
@@ -1,21 +1,38 @@
 package com.mygame;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.assets.AssetManager;
-import com.mygame.screen.GameScreen;
-
-public class Main extends Game {
-    public AssetManager assets;
+public class MyGdxGame implements ApplicationListener {
 
     @Override
     public void create() {
-        assets = new AssetManager();
-        setScreen(new GameScreen(this));
+        
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void pause() {
+        
+    }
+
+    @Override
+    public void resume() {
+        
     }
 
     @Override
     public void dispose() {
-        assets.dispose();
-        super.dispose();
+        
     }
 }
