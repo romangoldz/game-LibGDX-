718a77e52989ff3cdd66aaf4143caa276013579f
romangoldz <roman.goldzitskiy.2011@gmail.com>
2026-09-19 22:56:12 +0300

    Initialize SpriteBatch and Texture correctly
    Fix syntax errors in SpriteBatch and Texture initialization.

--- core/src/main/java/com/mygame/Main.java
+++ core/src/main/java/com/mygame/Main.java
@@ -1,13 +1,17 @@
 package com.mygame;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main implements ApplicationListener {
-
+    SpriteBatch batch;
+    Texture img; 
     @Override
     public void create() {
-        
+        batch = new SpriteBatch;
+        img = new Texture ("images/images1.png")
     }
 
     @Override
@@ -17,7 +21,7 @@ public class Main implements ApplicationListener {
 
     @Override
     public void render() {
-        
+        batch.draw(img, 100, 100, 200, 200);
 
         if (Gdx.input.isTouched()) {
             ScreenUtils.clear(0.2f, 0.1f, 0.2f, 1f);
